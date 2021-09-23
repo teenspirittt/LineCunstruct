cb5801084be031fc3a2292d8e492fba65334cc36
vladious7 <81105296+vladious7@users.noreply.github.com>
2021-09-23 17:08:14 +0700

    first commit

---
+++ lines.cpp
@@ -0,0 +1,5 @@
+//
+// Created by vladi on 23.09.2021.
+//
+
+#include "lines.h"
---
+++ lines.h
@@ -0,0 +1,8 @@
+//
+// Created by vladi on 23.09.2021.
+//
+
+#ifndef GRAFIC_LAB1__LINES_H_
+#define GRAFIC_LAB1__LINES_H_
+
+#endif //GRAFIC_LAB1__LINES_H_
